b427516339ad2f3b43ce3c6f9278d420391d1f71
Vincent Herreros <vincent-herreros@hotmail.fr>
2018-11-03 08:34:35 +0100

    [add] refractor size in string

--- src/main/scala/clicks/DataCleaner.scala
+++ src/main/scala/clicks/DataCleaner.scala
@@ -1,4 +1,4 @@
-val data = spark.read.json("/Users/johan/Downloads/data-students.json")
+val data = spark.read.json("/home/herreros/Téléchargements/data-students.json")
 val cleanBidFloor = data.na.fill(0, Seq("bidFloor"))
 
 val cleanNullInterests = cleanBidFloor.na.fill("NC", Seq("interests"))
@@ -21,5 +21,8 @@ def cleanInterests(col: org.apache.spark.sql.Column): org.apache.spark.sql.Colum
 
 val renameInterests = cleanSize.withColumn("interests", cleanInterests(col("interests")))
 
+val sizeToString = renameInterests.withColumn("size", concat_ws("x", $"size"))
+
+
 // cleanSize.coalesce(1).write.json("/Users/johan/Downloads/data-students-new1.json")
-renameInterests.coalesce(1).write.json("/Users/johan/Downloads/data-students-new2")
+sizeToString.coalesce(1).write.json("/home/herreros/Téléchargements/data-students-new2")
