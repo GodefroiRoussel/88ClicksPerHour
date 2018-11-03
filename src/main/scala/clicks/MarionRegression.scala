06169f312e119510c257da5672761e8227043b83
Marion <marion.mary@hotmail.fr>
2018-11-03 16:38:21 +0100

    [update] regression

--- src/main/scala/clicks/MarionRegression.scala
+++ src/main/scala/clicks/MarionRegression.scala
@@ -1,10 +1,11 @@
-def trainModel() = {
+def trainModel()  = {
 
     import org.apache.spark.ml._
 	import org.apache.spark.ml.attribute._
 	import org.apache.spark.ml.feature._
 	import org.apache.spark.ml.classification._
 	import org.apache.spark.ml.regression.GBTRegressor
+
 	val data = spark.read.json("Users/Marion/Desktop/data-students-new2/data.json")
 
 	val appOrSiteIndexer = new StringIndexer().setInputCol("appOrSite").setOutputCol("appOrSiteIndex")
@@ -15,15 +16,31 @@ def trainModel() = {
 	val timestampIndexer = new StringIndexer().setInputCol("timestamp").setOutputCol("timestampIndex")
 	val userIndexer = new StringIndexer().setInputCol("user").setOutputCol("userIndex")
 
-	val stringIndexers = Array(appOrSiteIndexer, interestsIndexer, mediaIndexer, publisherIndexer, sizeIndexer, timestampIndexer, userIndexer)
-
 	val encoder = new OneHotEncoderEstimator().setInputCols(Array("appOrSiteIndex", "interestsIndex", "mediaIndex", "publisherIndex", "sizeIndex", "timestampIndex", "userIndex"))
 	.setOutputCols(Array("appOrSiteEnc", "interestsEnc", "mediaEnc", "publisherEnc", "sizeEnc", "timestampEnc", "userEnc"))
 
 	val assembler = new VectorAssembler().setInputCols(Array("bidFloor", "appOrSiteEnc", "interestsEnc", "mediaEnc", "publisherEnc", "sizeEnc", "timestampEnc", "userEnc"))
 	.setOutputCol("features")
 
-	//val labelStringIndexer = new StringIndexer().setInputCol("label").setOutputCol("label")
+    val df2 = assembler.transform(data)
+    df2.show
+    val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("label")
+    val df3 = labelIndexer.fit(df2).transform(df2)
+    df3.show
+    val Array(trainingData, testData) = df3.randomSplit(Array(0.8, 0.2))
+
+    // Train the model
+    val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+    val model = lr.fit(trainingData)   
+    println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}")
+
+    // Test the model
+    val predictions = model.transform(testData)
+    predictions.show
+}
+
+// ANCIEN
+/**
 	val labelColumn = "label"
 	val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2))
 
@@ -43,7 +60,6 @@ def trainModel() = {
     /** val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
     val model = lr.fit(trainingData) 
     println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}") **/
-}
 
 
 
