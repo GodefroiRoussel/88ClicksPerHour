de40e9048fcb24395a5d7479afc087f5c88e244c
assilelyahyaoui <elyahyaouiassil@hotmail.com>
2018-11-12 22:54:34 +0100

    Cleaner updated

--- src/main/scala/clicks/DataCleaner.scala
+++ src/main/scala/clicks/DataCleaner.scala
@@ -1,46 +1,82 @@
-/*import org.apache.spark
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.ml.feature.OneHotEncoderEstimator
-import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.ml.feature.StringIndexerModel
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.SparkSession
+package clicks
 
-val spark = SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.regexp_replace
+import org.apache.spark.sql.Column
+import org.apache.spark.sql._
+
+
+object DataCleaner extends App {
+  val spark = SparkSession
     .builder()
-    .appName("Spark SQL basic example")
-    .config("spark.some.config.option", "some-value")
+    .appName("Click prediction")
+    .config("spark.master", "local")
     .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
 
-val data = spark.read.json("/home/godefroi/Téléchargements/data-students-new.json")
+  val data = spark.read.json("/Users/assilelyahyaoui/Documents/data-students.json")
 
-val labelAsInt = data.withColumn("label",$"label".cast("Int"))
+  def booleanToInt( bool :Boolean) = if(bool) 1 else 0
+  val booleanToInt_column = udf(booleanToInt _)
 
-val cleanBidFloor = labelAsInt.na.fill(0, Seq("bidFloor"))
+  /**
+    * transform label attribute to int
+    *
+    * @param dataFrame DataFrame to change
+    */
+  def label(dataFrame: DataFrame): DataFrame = {
+    dataFrame.withColumn("label", booleanToInt_column(dataFrame("label")))
+  }
 
-val cleanNullInterests = cleanBidFloor.na.fill("NC", Seq("interests"))
+  def cleanBidFloor(dataFrame: DataFrame) : DataFrame ={
+    dataFrame.na.fill(0,Seq("bidFloor"))
+  }
 
-val cleanSize = cleanNullInterests.filter("size is not NULL").select(
-    col("appOrSite"),
-    col("bidFloor"),
-    col("interests"),
-    col("label"),
-    col("media"),
-    col("publisher"),
-    col("size"),
-    col("timestamp"),
-    col("user")
-)
+  def cleanNullInterests(dataFrame: DataFrame) : DataFrame ={
+    dataFrame.na.fill("NC",Seq("interests"))
+  }
 
-def cleanInterests(col: org.apache.spark.sql.Column): org.apache.spark.sql.Column = {
+
+  def cleanSize(dataFrame: DataFrame):DataFrame ={
+    dataFrame.filter("size is not NULL").select(
+      col("appOrSite"),
+      col("bidFloor"),
+      col("interests"),
+      col("label"),
+      col("media"),
+      col("publisher"),
+      col("size"),
+      col("user")
+    )
+
+  }
+
+  def cleanInterestsRegex(col: org.apache.spark.sql.Column): org.apache.spark.sql.Column = {
     regexp_replace(col, "-[0-9]", "")
+  }
+
+
+  def cleanInterests(dataFrame: DataFrame) : DataFrame={
+    dataFrame.withColumn("interests", cleanInterestsRegex(col("interests")))
+  }
+
+  def sizeToString(dataFrame: DataFrame): DataFrame={
+    dataFrame.withColumn("size", concat_ws("x", $"size"))
+  }
+
+  def newDf(dataFrame: DataFrame): DataFrame ={
+    var ndf = dataFrame
+    ndf = label(ndf)
+    ndf = cleanBidFloor(ndf)
+    ndf = cleanNullInterests(ndf)
+    ndf = cleanSize(ndf)
+    ndf = cleanInterests(ndf)
+    ndf = sizeToString(ndf)
+    return ndf
+  }
+
+  //sizeToString.coalesce(1).write.json("/Users/assilelyahyaoui/Documents/data-students-cleaned.json")
+  // cleanSize.coalesce(1).write.json("/Users/johan/Downloads/data-students-new1.json")
+
 }
-
-val renameInterests = cleanSize.withColumn("interests", cleanInterests(col("interests")))
-
-val sizeToString = renameInterests.withColumn("size", concat_ws("x", $"size"))
-
-// cleanSize.coalesce(1).write.json("/Users/johan/Downloads/data-students-new1.json")
-sizeToString.coalesce(1).write.json("/home/godefroi/Téléchargements/new2JSON.json")
-*/
--- src/main/scala/clicks/Regression.scala
+++ src/main/scala/clicks/Regression.scala
@@ -1,104 +1,103 @@
-import org.apache.spark
+package clicks
+
 import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
-import org.apache.spark.sql.DataFrame
 import org.apache.spark.ml.feature._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.implicits._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object ClickPrediction extends App {
-    val spark = SparkSession
-        .builder()
-        .appName("Click prediction")
-        .config("spark.master", "local")
-        .getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
+  val spark = SparkSession
+    .builder()
+    .appName("Click prediction")
+    .config("spark.master", "local")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
 
-    val data = spark.read.json("/home/godefroi/Téléchargements/new2JSON.json")
+  val df = spark.read.json("/home/godefroi/Téléchargements/new2JSON.json")
+
+  val data = DataCleaner.newDf(df)
+
+  def indexStringColumns(df : DataFrame, col: String) : DataFrame = {
+    var newdf : DataFrame = df
+    val si : StringIndexer = new StringIndexer().setInputCol(col).setOutputCol(col + "Index")
+    val sm: StringIndexerModel = si.fit(newdf)
+    val indexed : DataFrame = sm.transform(newdf).drop(col)
+    newdf = indexed
+
+    return newdf
+  }
 
 
-    def indexStringColumns(df : DataFrame, col: String) : DataFrame = {
-        var newdf : DataFrame = df
-        val si : StringIndexer = new StringIndexer().setInputCol(col).setOutputCol(col + "Index")
-        val sm: StringIndexerModel = si.fit(newdf)
-        val indexed : DataFrame = sm.transform(newdf).drop(col)
-        newdf = indexed
 
-        return newdf
-    }
+  // One hot encoder estimator
+  // Maps a column of label indices to a column of binary vectors, with at most a single one-value. That alows Logistic Regression to use categorical features
+  def oneHot(df : DataFrame, cols: Array[String]) : DataFrame ={
 
+    var newdf : DataFrame = df
 
+    for (col <- cols){
 
-    // One hot encoder estimator
-    // Maps a column of label indices to a column of binary vectors, with at most a single one-value. That alows Logistic Regression to use categorical features
-    def oneHot(df : DataFrame, cols: Array[String]) : DataFrame ={
-
-        var newdf : DataFrame = df
-
-        for (col <- cols){
-
-            val oh : OneHotEncoderEstimator = new OneHotEncoderEstimator()
-                .setInputCols(Array(col))
-                .setOutputCols(Array(col + "Enc"))
-            val model : OneHotEncoderModel = oh.fit(newdf)
-            val encoded : DataFrame = model.transform(newdf).drop(col)
-            newdf = encoded
-
-        }
-        return newdf
+      val oh : OneHotEncoderEstimator = new OneHotEncoderEstimator()
+        .setInputCols(Array(col))
+        .setOutputCols(Array(col + "Enc"))
+      val model : OneHotEncoderModel = oh.fit(newdf)
+      val encoded : DataFrame = model.transform(newdf).drop(col)
+      newdf = encoded
 
     }
+    return newdf
 
-    val bidFloorIndexer: DataFrame = indexStringColumns(data, "bidFloor")
-    val appOrSiteIndexer: DataFrame = indexStringColumns(bidFloorIndexer, "appOrSite")
-    val interestsIndexer: DataFrame = indexStringColumns(appOrSiteIndexer, "interests")
-    val mediaIndexer: DataFrame = indexStringColumns(interestsIndexer, "media")
-    val publisherIndexer: DataFrame = indexStringColumns(mediaIndexer, "publisher")
-    val sizeIndexer: DataFrame = indexStringColumns(publisherIndexer, "size")
-    val timestampIndexer: DataFrame = indexStringColumns(sizeIndexer, "timestamp")
-    val userIndexer: DataFrame = indexStringColumns(timestampIndexer, "user")
-    println("-----------------------------         USER INDEXER               ------------------------------------------------------")
-    userIndexer.printSchema()
-    userIndexer.show()
+  }
 
-/*
-    val encoder : DataFrame = oneHot(userIndexer, Array("bidFloorIndex", "appOrSiteIndex", "interestsIndex", "mediaIndex", "publisherIndex", "sizeIndex" ,"timestampIndex", "userIndex"))
-    println(" -----------------------------          ENCODER               ------------------------------------------------------")
-    encoder.printSchema()
-    encoder.show()*/
+  val appOrSiteIndexer: DataFrame = indexStringColumns(data, "appOrSite")
+  val interestsIndexer: DataFrame = indexStringColumns(appOrSiteIndexer, "interests")
+  val mediaIndexer: DataFrame = indexStringColumns(interestsIndexer, "media")
+  val publisherIndexer: DataFrame = indexStringColumns(mediaIndexer, "publisher")
+  val sizeIndexer: DataFrame = indexStringColumns(publisherIndexer, "size")
+  val userIndexer: DataFrame = indexStringColumns(sizeIndexer, "user")
 
-    val test = 0.25
-    val training = 0.75
-    val splits : Array[DataFrame] = userIndexer.randomSplit(Array(training, test))
-	  val trainData : DataFrame = splits(0)
-	  val testData : DataFrame = splits(1)
+  println("-----------------------------         USER INDEXER               ------------------------------------------------------")
+  userIndexer.printSchema()
+  userIndexer.show()
+
+  val encoder : DataFrame = oneHot(userIndexer, Array("appOrSiteIndex", "interestsIndex", "mediaIndex", "publisherIndex", "sizeIndex" , "userIndex"))
+  println(" -----------------------------          ENCODER               ------------------------------------------------------")
+  encoder.printSchema()
+  encoder.show()
+
+  val test = 0.25
+  val training = 0.75
+  val splits : Array[DataFrame] = userIndexer.randomSplit(Array(training, test))
+  val trainData : DataFrame = splits(0)
+  val testData : DataFrame = splits(1)
+  
+  val assembler: VectorAssembler = new VectorAssembler()
+    .setInputCols(Array("bidFloor", "appOrSiteIndex", "interestsIndex", "mediaIndex", "publisherIndex", "sizeIndex", "userIndex"))
+    .setOutputCol("features")
+
+  val OneHotTRAIN: DataFrame = assembler.transform(trainData)
+  OneHotTRAIN.printSchema()
+
+  val OneHotTEST: DataFrame = assembler.transform(testData)
+  OneHotTEST.printSchema()
+  // Train the model
+
+  val lr: LogisticRegression = new LogisticRegression()
+    .setLabelCol("label")
+    .setFeaturesCol("features")
+    .setPredictionCol("prediction")
+    .setMaxIter(10)
+    .setRegParam(0.3)
+    .setThreshold(0.5)
+    .setFamily("auto")
+
+  val model: LogisticRegressionModel = lr.fit(OneHotTRAIN)
+  println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}")
 
 
-    val assembler: VectorAssembler = new VectorAssembler()
-        .setInputCols(Array("bidFloorIndex", "appOrSiteIndex", "interestsIndex", "mediaIndex", "publisherIndex", "sizeIndex", "timestampIndex", "userIndex"))
-        .setOutputCol("features")
+  // Test the model
+  val predictions: DataFrame = model.transform(OneHotTEST)
+  predictions.show
 
-    val OneHotTRAIN: DataFrame = assembler.transform(trainData)
-    OneHotTRAIN.printSchema()
-
-    val OneHotTEST: DataFrame = assembler.transform(testData)
-    OneHotTEST.printSchema()
-        // Train the model
-
-    val lr: LogisticRegression = new LogisticRegression()
-        .setLabelCol("label")
-        .setFeaturesCol("features")
-        .setPredictionCol("prediction")
-        .setMaxIter(10)
-        .setRegParam(0.3)
-        .setThreshold(0.5)
-        .setFamily("auto")
-
-    val model: LogisticRegressionModel = lr.fit(OneHotTRAIN)
-    println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}")
-
-
-    // Test the model
-    val predictions: DataFrame = model.transform(OneHotTEST)
-    predictions.show
-
-    spark.close()
+  spark.close()
 }
